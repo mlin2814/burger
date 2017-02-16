43320efbc2f4847f69ccd9acaeace8f0be05d886
Matthew Lin <Matthew Lin>
2017-02-15 23:03:01 -0500

    Clean install

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO flashlights (name, price, modes, lumens, power, link) VALUES ("Streamlight Microstream", 17.95, "Single", 45, "AAA", "http://www.streamlight.com/en/products/detail/index/microstream" );
+INSERT INTO flashlights (name, price, modes, lumens, power, link) VALUES ("Lumintop Tool", 18.90, "Multiple", 5-32-100, "AAA", "http://www.lumintop.com/tool-aaa.html");
+INSERT INTO flashlights (name, price, modes, lumens, power, link) VALUES ("Olight S1 Baton", 55.00, "Multiple", 0.5-500, "CR123", "https://olightworld.com/store/flashlight/s1-baton.html");
+INSERT INTO multitool (name, price, clamp, size, link) VALUES ("Victorinox Tinker", 30.00, "Blade", "Pocket", "https://www.swissarmy.com/us/en/Products/Swiss-Army-Knives/Medium-Pocket-Knives/Tinker/p/1.4603");
+INSERT INTO multitool (name, price, clamp, size, link) VALUES ("Leatherman Wave", 90.85, "Pliers", "Waist Pouch / Bag", "https://www.leatherman.com/wave-10.html");
+INSERT INTO packs (name, price, look, size, style, link) VALUES ("5.11 Rush 12", 99.99, "Tactical", "Daypack", "Backpack", "http://www.511tactical.com/rush-12-backpack.html");
